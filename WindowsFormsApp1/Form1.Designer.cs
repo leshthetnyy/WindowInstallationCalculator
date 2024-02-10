14edd181cff5509017aba535b38b659802620033
Windows <Windows@DESKTOP-C7208KC>
2024-02-10 23:46:57 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Окна",
+            "Балконы",
+            "Двери"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(175, 25);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(47, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Услуга";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.textBox1.Location = new System.Drawing.Point(140, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.textBox2.Location = new System.Drawing.Point(140, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label2.Location = new System.Drawing.Point(47, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ширина (м)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label3.Location = new System.Drawing.Point(55, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Высота (м)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(50, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Вывод рассчетов:";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button1.Location = new System.Drawing.Point(50, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.button2.Location = new System.Drawing.Point(50, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(427, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Оформить квитанцию";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.radioButton1.Location = new System.Drawing.Point(43, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Глухое";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(277, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 167);
+            this.panel1.TabIndex = 10;
+            this.panel1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.radioButton2.Location = new System.Drawing.Point(43, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Поворотное";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.radioButton3.Location = new System.Drawing.Point(43, 80);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(90, 21);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Откидное";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.radioButton4.Location = new System.Drawing.Point(43, 107);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(106, 21);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Фрамужное";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.radioButton5.Location = new System.Drawing.Point(43, 134);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(105, 21);
+            this.radioButton5.TabIndex = 13;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Раздвижное";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(29, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Выберите тип окна";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(50, 259);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(427, 86);
+            this.panel2.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.label6.Location = new System.Drawing.Point(5, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Окна";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // переменные цен за квадратный метр для различных элементов
+        double WindowPriceMeter = 2000.00;
+        double BalconyPriceMeter = 1500.00;
+        double DoorPriceMeter = 1800.00;
+        double TotalCost = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList; // запрет ввода в comboBox
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // ввод только цифр и одной запятой в textBox
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true; 
+            }
+            TextBox textBox = sender as TextBox;
+            if (e.KeyChar == ',')
+            {
+                if (textBox.Text.Contains(","))
+                {
+                    e.Handled = true; 
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // проверка: выбрана ли услгуа
+            if (comboBox1.Text != "")
+            {
+                // проверка: введены ли значения
+                if (textBox1.Text != "" && textBox1.Text != "")
+                {
+                   
+                        // берем значения, которые ввел пользователь
+                        double WidthMeter = Convert.ToDouble(textBox1.Text);
+                        double HeightMeter = Convert.ToDouble(textBox2.Text);
+
+                        // проверка: ввыбран ли элемент "окно"
+                        if (comboBox1.SelectedIndex == 0)
+                        {
+                            // проверка: ввыбран ли тип окна
+                            if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked || radioButton5.Checked)
+                            {
+                                double CostTypeWindow = 0; // данная переменная будет хранить, стоимость за тип окна
+
+                                // проверка: какой выбран тип окна
+                                if (radioButton1.Checked)
+                                {
+                                    CostTypeWindow = 1000.00; // глухое
+                                }
+                                else if (radioButton2.Checked)
+                                {
+                                    CostTypeWindow = 3400.50; // поворотное
+                                }
+                                else if (radioButton3.Checked)
+                                {
+                                    CostTypeWindow = 2560.00; // откидное
+                                }
+                                else if (radioButton4.Checked)
+                                {
+                                    CostTypeWindow = 7900.90; // фрамужное
+                                }
+                                else if (radioButton5.Checked)
+                                {
+                                    CostTypeWindow = 6210.50; // раздвижное
+                                }
+
+                                TotalCost = ((WidthMeter * HeightMeter) * WindowPriceMeter) + CostTypeWindow;
+                                label6.Text = "Итого за окно: " + Convert.ToString(TotalCost) + " рублей.";
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Не выбран тип окна!\nВыберите тип окна!");
+                            }
+                        }
+
+                        if (comboBox1.SelectedIndex == 1)
+                        {
+                            TotalCost = ((WidthMeter * HeightMeter) * BalconyPriceMeter);
+                            label6.Text = "Итого за балкон: " + Convert.ToString(TotalCost) + " рублей.";
+
+                        }
+                        if (comboBox1.SelectedIndex == 2)
+                        {
+                            TotalCost = ((WidthMeter * HeightMeter) * DoorPriceMeter);
+                            label6.Text = "Итого за дверь: " + Convert.ToString(TotalCost) + " рублей.";
+                        }
+                        }         
+                else
+                {
+                    MessageBox.Show("Значения не введены!\nВведите все значеня!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Услуга не выбрана!\nВыберите услугу!");
+            }
+
+            // если все верно, то можно сформировать квитанцию
+            if (comboBox1.Text != "" && label6.Text != "" && textBox1.Text != "" && textBox2.Text != "")
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // вывод панели выбора типа окна
+            if (comboBox1.SelectedIndex == 0)
+            {
+                panel1.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // создаем объект приложения Word
+            Word.Application app = new Word.Application();
+
+            // создаем новый документ на основе шаблона
+            Word.Document doc = app.Documents.Add(@"C:\Users\anton\Desktop\Project\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Квитанции\ШаблонКвитанции.docx");
+
+            string UniqueNumber = Guid.NewGuid().ToString(); // уникальный номер
+            string CurrentDate = DateTime.Now.ToString("ddMMyyyy"); // дата (только цифры для названия документа)
+            string CurrentDateTry = DateTime.Now.ToString("dd.MM.yyyy"); // дата (для отображения в документе)
+
+            // Вставляем данные в определенные поля в документе
+            doc.Bookmarks["Закладка_1"].Range.Text = UniqueNumber;
+            doc.Bookmarks["Закладка_2"].Range.Text = Convert.ToString(CurrentDateTry);
+            doc.Bookmarks["Закладка_3"].Range.Text = Convert.ToString(comboBox1.Text);
+            doc.Bookmarks["Закладка_4"].Range.Text = Convert.ToString(TotalCost) + " рублей";
+
+            //Сохраняем документ с заданным именем
+            string NewNameDoc = UniqueNumber + "_" + CurrentDate + "_" + Convert.ToString(TotalCost)+"рублей";
+            string NameDoc = NewNameDoc + ".docx";
+            object PathDoc = @"C:\Users\anton\Desktop\Project\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\Квитанции\" + NameDoc;
+            doc.SaveAs2(PathDoc);
+            string PathDocString = Convert.ToString(PathDoc);
+            app.Documents.Open(PathDocString);
+        }
+    }
+}
